b30912f77269499a9c24cc3cf5215fe3e2f7866f
Shubhnkar <shubh800@gmail.com>
2015-12-01 22:49:06 +0530

    minimum value in the BST

---
+++ min_val_bt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by babayega on 11/30/15.
+//
+
+#include "min_val_bt.h"
---
+++ min_val_bt.h
@@ -0,0 +1,14 @@
+//
+// Created by babayega on 11/30/15.
+//
+
+#ifndef GEEKSSOL_MIN_VAL_BT_H
+#define GEEKSSOL_MIN_VAL_BT_H
+
+
+class min_val_bt {
+
+};
+
+
+#endif //GEEKSSOL_MIN_VAL_BT_H
